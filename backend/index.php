79a3d9c44c8f59c2ddbce2d2952d9b0a50c3ebd2
gerhard <gerhard@76a09a1d-e961-4c31-8475-84ec11d7c969>
2010-07-14 12:37:34 +0000

    now with picturegallery support
    git-svn-id: https://svn.liip.ch/repos/liip/misc/niweaapp/trunk@40307 76a09a1d-e961-4c31-8475-84ec11d7c969

--- backend/index.php
+++ backend/index.php
@@ -3,7 +3,7 @@
 ini_set("display_errors", 1);
 error_reporting (E_ALL);
 
-header("Content-Type: application/json");
+
 
 class tagiProxy {
 
@@ -15,10 +15,11 @@ class tagiProxy {
 	public $mode;
 	private $rss;
 	private $items = array();
+	private $galleries = array();
 	
 	public function getJsonByCategoryId($id, $mode){
 		if($this->parseInput($id, $mode)){			
-			$this->getRssByCategoryId($this->id, $this->mode);	
+			$this->getRssById($this->id, $this->mode);	
 		}		
 	}
 	
@@ -51,13 +52,13 @@ class tagiProxy {
 	}
 	
 	
-	private function getRssByCategoryId(){		
+	private function getRssById(){		
 		
 		$feedvar = $this->mode."feed";
 		$this->rss = simplexml_load_file("{$this->$feedvar}{$this->id}");
 		$this->rss2Array();
 		
-		// header("Content-type: application/json");
+		header("Content-type: application/json");
 		echo $this->array2Json();
 	}
 	
@@ -65,26 +66,67 @@ class tagiProxy {
 	private function array2Json(){
 		$overall = array();
 		$overall['id'] = $this->id;
-		$overall['items'] = $this->items;		
+		$overall['items'] = $this->items;
+		$overall['galleries'] = $this->galleries;		
 		return(json_encode($overall));
 	}
 	
 	private function rss2Array(){
 		// vereinfachen, shorten
 		
+		
 		foreach($this->rss->channel->item as $item){
+			// do nothing with picturegalleries right now.. first just articles
+			if((string)$item->attributes()->type == "picturegalleries"){
+				$galleries = $item;
+				break;
+			}
+		
 			$lead = $item->lead;
 			$shortlead = substr($lead, 0, 110)."...";
-			
 			$itm = array();
-				foreach($item as $k => $v){
+			foreach($item as $k => $v){
+				
+				if($k == "recent_stories" ){
+					$ar = array();
+					foreach($v->item as $_k => $_v){
+						array_push($ar, array("id" => trim((string)$_v->id), "title" => trim((string)$_v->title)));
+					}
+					$itm[$k] = $ar;
+					
+				} else if($k == "context_stories"){
+					$ar = array();
+					foreach($v->context as $_k => $_v){
+						array_push($ar, array("context_id" => trim((string)$_v->context_id), "context_title" => trim((string)$_v->context_title), "context_url" => trim((string)$_v->context_url)));
+					}
+					$itm[$k] = $ar;
+				}
+				
+				else{
 					$itm[$k] = trim((string)$v);
 				}
+				
+				
+			}
 			$itm['shortlead'] = $shortlead;
-			array_push($this->items, $itm);
+			array_push($this->items, $itm);			
 		}
 		
-		return($this->items);
+		
+		
+		if(isset($galleries)){
+			foreach($galleries->item as $k => $v){
+				array_push($this->galleries, array(
+					"id" => trim((string)$v->id),
+					"title" => trim((string)$v->title),
+					"subtitle" => trim((string)$v->subtitle),
+					"link" => trim((string)$v->link),
+					"views" => trim((string)$v->views),
+					"pubDate" => trim((string)$v->pubDate),
+					"image_thumbnail" => trim((string)$v->image_thumbnail),
+					"image_thumbnail_ipad" => trim((string)$v->image_thumbnail_ipad)));
+			}
+		}
 	}
 	
 	private function throwError($key){
